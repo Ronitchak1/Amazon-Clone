5c76333f628765a5e6ba39e24722b08ae65bbb54
Ronitchak1 <47414461+Ronitchak1@users.noreply.github.com>
2022-06-10 20:34:32 +0530

    Updated Login component and added styling

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../styles/Login.css";
 import { Link } from "react-router-dom";
 
 function Login() {
+
+  const [email,setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const signIn = (e) => {
+    e.preventDefault();
+
+    //Firebase Login 
+  }
+
+  const register = (e) => {
+    e.preventDefault();
+
+    //Firebase Register
+  }
+
   return (
     <div className="login">
         <Link to="/">
@@ -14,7 +30,23 @@ function Login() {
         </Link>
 
         <div className="login__container">
-                <h1>Sign In</h1>
+          <h1>Sign In</h1>
+          <form>
+              <h5>E-mail</h5>
+              <input type="text" value={email} onChange={e => setEmail(e.target.value)}/>
+
+              <h5>Password</h5>
+              <input type="password" value={password} onChange={e => setEmail(e.target.value)}/>
+
+              <button className='login__signInButton' type='submit' onClick={signIn}>Sign In</button>
+          </form>
+
+          <p>
+            By signing-in you agree to AMAZON FAKE CLONE Conditons
+            of Use & Sale. Please see our Privacy Notice, our
+            Cookies Notice and our interest-Based Ads Notice.
+          </p>
+          <button className='login__registerButton' onClick={register}>Create your Amazon Account</button>
         </div>
     </div>
   )
