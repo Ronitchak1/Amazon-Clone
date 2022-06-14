591c50136d6e811ba2fc157e5e4c7cad11ef7700
Ronitchak1 <47414461+Ronitchak1@users.noreply.github.com>
2022-06-14 18:59:25 +0530

    Completed Payment.js and included Axios

--- src/App.js
+++ src/App.js
@@ -1,49 +1,52 @@
 import React, { useEffect } from "react";
-import './App.css';
-import Header from './components/Header';
-import Home from './components/Home';
-import Checkout from './components/Checkout';
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Checkout from "./components/Checkout";
 import Payment from "./components/Payment";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import { auth } from "./firebase";
 import { useStateValue } from "./components/StateProvider";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe(
+  "pk_test_51L9mEFSFJKcXfGZ11D8EriXd8Tm4bBibwL3f99n8GzlKJCsL79vVvnRQYDtjpjhYixiNvn3tANuIBrQnqjHYFCAe00W44kJAAG"
+);
 
 export default function App() {
-
   const [state, dispatch] = useStateValue();
 
   useEffect(() => {
     //Will only run once when the app component loads...
-    auth.onAuthStateChanged(authUser => {
-      
-      if(authUser){
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
         //The User just/was logged in
         dispatch({
-          type: 'SET_USER',
-          user: authUser
-        })
-      }
-      else{
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
         //The User is logged out
         dispatch({
-          type: 'SET_USER',
-          user: null
-        })
+          type: "SET_USER",
+          user: null,
+        });
       }
-    })
-  }, [])
+    });
+  }, []);
 
   return (
     <Router>
-    <div className="app">
-      <Routes>
-        <Route path="/login" element={<LoginPage/>}/>
-        <Route path="/" element={<HomePage/>} /> 
-        <Route path="/checkout" element={<CheckoutPage/>} /> 
-        <Route path="/payment" element={<PaymentPage/>}/>
-      </Routes>
-    </div>
+      <div className="app">
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/payment" element={<PaymentPage />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
@@ -51,36 +54,36 @@ export default function App() {
 const LoginPage = () => {
   return (
     <>
-      <Login/>
+      <Login />
     </>
   );
-}
+};
 
 const HomePage = () => {
-  return(
-    <>
-      <Header/>
-      <Home/>
-    </>
-  );
-}
-
-const CheckoutPage = () => {
-  return(
-    <>
-      <Header/>
-      <Checkout/>
-    </>
-  );
-}
-
-const PaymentPage = () =>{
   return (
     <>
-      <Header/>
-      <Payment/>
+      <Header />
+      <Home />
     </>
   );
-}
+};
 
+const CheckoutPage = () => {
+  return (
+    <>
+      <Header />
+      <Checkout />
+    </>
+  );
+};
 
+const PaymentPage = () => {
+  return (
+    <>
+      <Header />
+      <Elements stripe={promise}>
+        <Payment />
+      </Elements>
+    </>
+  );
+};
--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,11 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "./axios";
 import "../styles/Payment.css";
 import CheckoutProduct from "./CheckoutProduct";
 import { useStateValue } from "./StateProvider";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import CurrencyFormat from "react-currency-format";
+import { getBasketTotal } from "./reducer";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 
 function Payment() {
+  const navigate = useNavigate();
+  const stripe = useStripe();
+  const elements = useElements();
+
   const [state, dispatch] = useStateValue();
+
+  const [succeeded, setSucceeded] = useState(false);
+  const [processing, setProcessing] = useState("");
+  const [error, setError] = useState(null);
+  const [disabled, setDisabled] = useState(true);
+  const [clientSecret, setClientSecret] = useState(true);
+
+  useEffect(() => {
+    //Generate the special stripe secret which allows us to charge a customer
+    const getClientSecret = async () => {
+      const response = await axios({
+        method: "post",
+        //Stripe expects the total in a currencies subunit
+        url: "/payments/create?total=${getBasketTotal(state.basket) * 100}",
+      });
+      setClientSecret(response.data.clientSecret);
+    };
+
+    getClientSecret();
+  }, [state.basket]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setProcessing(true);
+
+    const payload = await stripe
+      .confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: elements.getElement(CardElement),
+        },
+      })
+      .then(({ paymentIntent }) => {
+        //paymentIntent = Payment Confirmation
+
+        setSucceeded(true);
+        setError(null);
+        setProcessing(false);
+
+        navigate("/orders");
+      });
+  };
+
+  const handleChange = (e) => {
+    //Listen for changes in the CardElement
+    //and display any errors as the customer types their card details
+    setDisabled(e.empty);
+    setError(e.error ? e.error.message : "");
+  };
 
   return (
     <div className="payment">
@@ -44,7 +100,29 @@ function Payment() {
           <div className="payment__title">
             <h3>Payment Method</h3>
           </div>
-          <div className="payment__details"></div>
+          <div className="payment__details">
+            <form onSubmit={handleSubmit}>
+              <CardElement onChange={handleChange} />
+              <div className="payment__priceContainer">
+                <CurrencyFormat
+                  renderText={(value) => (
+                    <>
+                      <h3>Order Total: {value}</h3>
+                    </>
+                  )}
+                  decimalScale={2}
+                  value={getBasketTotal(state.basket)}
+                  displayType={"text"}
+                  thousandSeparator={true}
+                  prefix={"$"}
+                />
+                <button disabled={processing || disabled || succeeded}>
+                  <span>{processing ? <p>Processing</p> : "Buy Now"}</span>
+                </button>
+              </div>
+              {error && <div>{error}</div>}
+            </form>
+          </div>
         </div>
       </div>
     </div>
